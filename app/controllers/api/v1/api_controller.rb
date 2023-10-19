e60fede64ec8d149b1be13c5e7fad246fd30047c
devendrap1514 <141813743+devendrap1514@users.noreply.github.com>
2023-10-19 12:04:37 +0530

    Update api_controller.rb
    uncomment the api_controller

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -7,28 +7,28 @@ class Api::V1::ApiController < ApplicationController
   before_action :authorize_request
   authorize_resource
 
-  # def authorize_request
-  #   header = request.headers["Authorization"]
-  #   token = header.split(' ')[1] if header
+  def authorize_request
+    header = request.headers["Authorization"]
+    token = header.split(' ')[1] if header
 
-  #   unless session[:token]
-  #     respond_to do |format|
-  #       format.json { render status: :unauthorized, json: {message: "First Login"} }
-  #       format.html { redirect_to new_api_v1_user_authentication_path }
-  #     end
-  #     return
-  #   end unless token
+    unless session[:token]
+      respond_to do |format|
+        format.json { render status: :unauthorized, json: {message: "First Login"} }
+        format.html { redirect_to new_api_v1_user_authentication_path }
+      end
+      return
+    end unless token
 
-  #   begin
-  #     decoded = JsonWebToken.decode(token || session[:token])
-  #     @current_user = User.find(decoded[:user_id])
-  #   rescue ActiveRecord::RecordNotFound => e
-  #     session.delete(:token)
-  #     render status: :not_found, json: { message: e.message }
-  #   rescue JWT::DecodeError => e
-  #     render status: :unauthorized, json: { message: e.message }
-  #   end
-  # end
+    begin
+      decoded = JsonWebToken.decode(token || session[:token])
+      @current_user = User.find(decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      session.delete(:token)
+      render status: :not_found, json: { message: e.message }
+    rescue JWT::DecodeError => e
+      render status: :unauthorized, json: { message: e.message }
+    end
+  end
 
   def is_login?
     return true if session[:token]
